02814d37c78e1c56f3ab85e8d5a7e73fb801fced
VenkateshBattula45 <venkteshbathula225@gmail.com>
2025-07-16 02:09:46 +0530

    initil-commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Propos from './Props';
+import Data from './Data';
 
 function App() {
  return (
  <>
-<Propos name="venkatesh" age="22" qualification="B.Tech" city="Dachepalli"/>
-<Propos name="venkatesh" age="22" qualification="B.Tech" city="Dachepalli"/>
+ <section>
+    {
+        Data.map((eachObj)=>{
+            const {id,title,body}=eachObj;
+           return < Profile id={id} title={title} body={body} />
+        })
+    }
+ </section>
+ {/* <Propos name="venkatesh" age="22" qualification="B.Tech" city="Dachepalli"/> */}
+{/* <Profile/> */}
+{/* <Propos name="venkatesh" age="22" qualification="B.Tech" city="Dachepalli"/> */}
  </>
  ) 
+}
+
+const Profile =(props)=>{
+    const {id,title,body}=props;
+    return 
+    <article className='Student'>
+        <h1>{id}</h1>
+        <h2>{title}</h2>
+        <p>{body}</p>
+    </article>
+
 }
 export default App;
